20326403ab9b925b3443d4c950fa2a2dd9e13022
Jonathan Biemond <101326085+jonbiemond@users.noreply.github.com>
2022-12-03 14:44:40 -0800

    Describe steps

--- day-3/day3-solution.py
+++ day-3/day3-solution.py
@@ -3,12 +3,13 @@ with open('day-3/input.txt', 'r') as f:
 
 p_list = []
 
-for pack in input.split('\n'):
-    comp2 = pack[-int(len(pack)/2):]
-    for i in pack:
-        if i in comp2:
-            x = ord(i)
-            p_list+= [x - 96 if x > 96 else x - 38]
+for pack in input.split('\n'):  # split input into packs (lines)
+    comp2 = pack[-int(len(pack)/2):]  # grab the last half of the line
+    # go over each character in the line
+    for i in pack: 
+        if i in comp2:  # check for a match in the last half
+            x = ord(i)  # convert to ascii # code (a = 97 A = 65)
+            p_list += [x - 96 if x > 96 else x - 38]  # subtract to get the desired integer and add to list
             break
 
 print(f'Part one: {sum(p_list)}')
